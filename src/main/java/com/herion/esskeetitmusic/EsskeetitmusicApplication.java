f711ace2fd99f2a2e0e3103b2e1d83d9b0cd7684
Marcabo <1790043847@qq.com>
2019-07-16 00:33:04 +0800

    项目初始化.项目结构搭建

---
+++ src/main/java/com/herion/esskeetitmusic/EsskeetitmusicApplication.java
@@ -0,0 +1,13 @@
+package com.herion.esskeetitmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsskeetitmusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsskeetitmusicApplication.class, args);
+    }
+
+}
